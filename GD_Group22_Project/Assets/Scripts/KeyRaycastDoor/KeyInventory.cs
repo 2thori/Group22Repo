50a99b5db76e663049e8048e24c63b03a44b85ee
uNathi2 <Nkosinathititus1@outlook.com>
2025-08-19 12:56:37 +0200

    Door Mechanics v1

---
+++ GD_Group22_Project/Assets/Scripts/KeyRaycastDoor/KeyInventory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace KeySystem
+{ 
+    public class KeyInventory : MonoBehaviour
+{
+    public bool hasOrangeKey = false;
+}
+    
+}
