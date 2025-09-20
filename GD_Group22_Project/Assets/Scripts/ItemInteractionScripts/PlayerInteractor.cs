bc92a15dbd5bec90ed4a6111c7b7991932c2df77
uNathi2 <Nkosinathititus1@outlook.com>
2025-09-20 13:45:54 +0200

    physics gun

--- GD_Group22_Project/Assets/Scripts/ItemInteractionScripts/PlayerInteractor.cs
+++ GD_Group22_Project/Assets/Scripts/ItemInteractionScripts/PlayerInteractor.cs
@@ -1,27 +1,87 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerInteractor : MonoBehaviour
 {
+    [Header("Player Setup")]
+    [Tooltip("The player's camera used for raycasting")]
     [SerializeField] private Camera playerCamera;
-    [SerializeField] private float interactRange = 3f; // How far player can reach
-    [SerializeField] private KeyCode interactKey = KeyCode.E;
 
-    void Update()
+    [Tooltip("How far the player can reach to interact")]
+    [SerializeField] private float interactRange = 3f;
+
+    [Tooltip("Input Action for interaction")]
+    [SerializeField] private InputActionReference interactAction;
+
+    [Header("Crosshair UI (optional)")]
+    [Tooltip("If using an Image crosshair, assign it here")]
+    [SerializeField] private Image crosshairImage;
+
+    [Tooltip("If using TextMeshPro crosshair, assign it here")]
+    [SerializeField] private TMP_Text crosshairText;
+
+    [Tooltip("Default color for crosshair")]
+    [SerializeField] private Color defaultColor = Color.white;
+
+    [Tooltip("Color when looking at an interactable object")]
+    [SerializeField] private Color highlightColor = Color.green;
+
+    private IInteractable currentInteractable;
+
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(interactKey))
-        {
-            Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
-            RaycastHit hit;
+        if (interactAction != null)
+            interactAction.action.performed += OnInteract;
+    }
 
-            if (Physics.Raycast(ray, out hit, interactRange))
+    private void OnDisable()
+    {
+        if (interactAction != null)
+            interactAction.action.performed -= OnInteract;
+    }
+
+    private void Update()
+    {
+        UpdateCrosshair();
+    }
+
+    private void UpdateCrosshair()
+    {
+        currentInteractable = null;
+
+        if (playerCamera == null)
+            return;
+
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, interactRange))
+        {
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+            if (interactable != null)
             {
-                // Check if object has IInteractable component
-                IInteractable interactable = hit.collider.GetComponent<IInteractable>();
-                if (interactable != null)
-                {
-                    interactable.Interact();
-                }
+                currentInteractable = interactable;
             }
+        }
+
+        // Update crosshair visuals
+        if (crosshairImage != null)
+            crosshairImage.color = currentInteractable != null ? highlightColor : defaultColor;
+
+        if (crosshairText != null)
+            crosshairText.color = currentInteractable != null ? highlightColor : defaultColor;
+    }
+
+    private void OnInteract(InputAction.CallbackContext ctx)
+    {
+        // Ignore if pointing at UI
+        if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (currentInteractable != null)
+        {
+            currentInteractable.Interact();
         }
     }
 }
