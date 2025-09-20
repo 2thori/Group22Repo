5537ea69b0e9251104c3fd64109881e04c98deb1
uNathi2 <Nkosinathititus1@outlook.com>
2025-09-20 21:17:53 +0200

    me

--- GD_Group22_Project/Assets/Scripts/DoorScripts/Key.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Key", menuName = "Scriptable Object/Key")]
-public class Key : ScriptableObject
-{
-    public string keyName;
-    public int id;
-    public Sprite keySprite;
-}
