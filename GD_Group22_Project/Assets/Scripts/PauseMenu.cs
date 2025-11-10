b3ca77c0849be5b978cd9b0d67da71b56c1bf9da
Tshidiso Moloi <1840064@students.wits.ac.za>
2025-11-10 02:31:43 +0200

    Pause Menu (Continued)
    Edits done on Game Scene (GrayBoxScene)
    - Pause Menu UI implemented and functional
    - Settings UI implemented
    * Volume functionality missing
    - Minor script changes

--- GD_Group22_Project/Assets/Scripts/PauseMenu.cs
+++ GD_Group22_Project/Assets/Scripts/PauseMenu.cs
@@ -1,43 +1,80 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] public static bool PausedGame = false;
+    [Header("UI Objects")]
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject gameHUD;
+    [SerializeField] private GameObject fpsHUD;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private Button menuButton;
+    [SerializeField] private Button optionsButton;
 
-    [SerializeField] public GameObject pauseMenu;
-    [SerializeField] public GameObject gameHUD;
-    [SerializeField] public GameObject fpsHUD;
+    [Header("Audio Objects")]
+    //[SerializeField]
+
+    [Header("Is the Game Paused?")]
+    [SerializeField] public bool PausedGame = false;
+    public static PauseMenu Instance { get; private set; }
 
     [SerializeField] public KeyCode pauseKey = KeyCode.Escape;
+
+    private void Awake()
+    {
+        Instance = this;
+        SetupButtonListeners();
+    }
 
     public void Update()
     {
         if (Input.GetKeyDown(pauseKey))
         {
-            if (PausedGame)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
+            TogglePause();
         }
+    }
+
+    void SetupButtonListeners()
+    { 
+        resumeButton.onClick.AddListener(Resume);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    public void TogglePause()
+    { 
+        PausedGame = !PausedGame;
+
+        pauseMenu.SetActive(PausedGame);
+        gameHUD.SetActive(!PausedGame);
+
+        Time.timeScale = PausedGame ? 0 : 1;
+
+        PlayerController player = FindObjectOfType<PlayerController>();
+        if (player != null) player.enabled = !PausedGame;
+
+        Cursor.lockState = PausedGame ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = PausedGame;
+
     }
 
     public void Resume()
     { 
+        /* Basic Resume functionality. Not functional ATM.
         Time.timeScale = 1f;
         gameHUD.SetActive(true);
         fpsHUD.SetActive(true);
         pauseMenu.SetActive(false);
         PausedGame = false;
-        //Cursor.lockState = CursorLockMode.Locked;
-
+        Cursor.lockState = CursorLockMode.Locked;
+        */
+        if (PausedGame) TogglePause();
     }
 
+    /* Another failed attempt
     public void Pause()
     {
         Time.timeScale = 0f;
@@ -45,9 +82,8 @@ public class PauseMenu : MonoBehaviour
         fpsHUD.SetActive(false);
         pauseMenu.SetActive(true);
         PausedGame = true;
-        //Cursor.lockState = CursorLockMode.None;
-        
-    }
+        Cursor.lockState = CursorLockMode.None;
+    }*/
 
     public void LoadMenu()
     {
